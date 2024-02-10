103daaf3d3591334d14ac86d6e1509317c79324e
Alin Florin <alin@qoffice.ro>
2024-02-10 12:20:22 +0200

    -

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,25 @@
-FROM alpine:latest
-
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
 ARG DEPLOYABLE_VERSION
 ARG TARGETPLATFORM
 
-ENTRYPOINT ["/bin/ash", "-c", "sleep infinity"]
+WORKDIR /app
+
+# copy csproj and restore as distinct layers
+COPY *.sln .
+COPY ./*.csproj .
+RUN dotnet restore
+
+# copy everything else and build app
+COPY . .
+RUN dotnet publish -c Release -o /app --no-restore
+
+# final stage/image
+FROM mcr.microsoft.com/dotnet/aspnet:8.0
+WORKDIR /app
+COPY --from=build /app ./
+ENTRYPOINT ["dotnet", "Huna.Signalr.dll"]
 
 EXPOSE 3000
 LABEL org.opencontainers.image.source=https://github.com/mars-office/huna-signalr
+
+
---
+++ Program.cs
@@ -0,0 +1,34 @@
+namespace Huna.Signalr;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+
+        var app = builder.Build();
+
+
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+        {
+            var forecast =  Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = summaries[Random.Shared.Next(summaries.Length)]
+                })
+                .ToArray();
+            return forecast;
+        })
+        .WithName("GetWeatherForecast");
+
+        app.Run("http://+:3000");
+    }
+}
