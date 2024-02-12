42bcfe1d74e96f004bc5c460bcd887a64925bc4d
Alin Florin <alin@qoffice.ro>
2024-02-13 00:23:44 +0200

    d

--- MqttHostedService.cs
+++ MqttHostedService.cs
@@ -20,7 +20,7 @@ namespace Huna.Signalr
             var clientCerts = new X509Certificate2Collection
             {
                 new X509Certificate2(
-                    X509Certificate2.CreateFromPem(_config["EMQX_CLIENT_CRT"]!, _config["EMQX_CLIENT_KEY"]! + "\n" + _config["EMQX_CA_CRT"]).Export(X509ContentType.Pfx)
+                    X509Certificate2.CreateFromPem(_config["EMQX_CLIENT_CRT"]! + "\n" + _config["EMQX_CA_CRT"], _config["EMQX_CLIENT_KEY"]!).Export(X509ContentType.Pfx)
                     )
             };
 
