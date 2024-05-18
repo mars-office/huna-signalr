de7ffdcb8db8ac2e197b70fdda09c8d32e3f5eec
Alin Florin <alin@qoffice.ro>
2024-05-18 13:03:41 +0300

    d

--- TestServuce.cs
+++ TestServuce.cs
@@ -18,14 +18,12 @@ namespace Huna.Signalr
 
         public async Task StartAsync(CancellationToken cancellationToken)
         {
-            for (var i = 0; i < 100; i++)
-            {
+       
                 await _bus.Publish(new NotificationRequest
                 {
                     Title = "xxxxxxx"
                 }, cancellationToken: cancellationToken);
-                await Task.Delay(5000, cancellationToken);
-            }
+
             Console.WriteLine("Doneeee");
         }
 
